fb771d2bfb8dd5c37d7beca3056effec84f8c4db
Aura Pasila <aura.pasila@gmail.com>
2025-11-18 18:18:53 +0200

    Reference entity

--- src/repositories/reference_repository.py
+++ src/repositories/reference_repository.py
@@ -4,11 +4,21 @@ from sqlalchemy import text
 from entities.reference import Reference
 
 def get_references():
+    """ Viitteiden haku, huomioi if-vaihtoehto jos ei viitteitä löydy tietokannasta """
     result = db.session.execute(text("SELECT author, title, year, publisher FROM references"))
     references = result.fetchall()
+
+    if not references:
+        return "Ei viitteitä"
+    
     return [Reference(reference[0], reference[1], reference[2], reference[3]) for reference in references] 
 
-def create_references(content):
-    sql = text("INSERT INTO references (content) VALUES (:content)")
-    db.session.execute(sql, { "content": content })
+def create_references(author, title, year, publisher):
+    """Viitteiden luominen tietokantaan"""
+    sql = text("INSERT INTO references (author, title, year, publisher) VALUES (:author, :title, :year, :publisher)")
+    db.session.execute(sql, { 
+        "author": author,
+        "title": title,
+        "year": year,
+        "publisher": publisher})
     db.session.commit()
