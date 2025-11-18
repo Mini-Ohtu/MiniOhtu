6b374096475e128aba34e525507f21838f59bbab
arppuri <anu.nissinen@helsinki.fi>
2025-11-18 18:56:06 +0200

    skeema uusi yritys

--- src/schema.sql
+++ src/schema.sql
@@ -3,3 +3,11 @@ CREATE TABLE todos (
   content TEXT NOT NULL,
   done BOOLEAN DEFAULT FALSE
 )
+
+CREATE TABLE references (
+  id SERIAL PRIMARY KEY, 
+  author TEXT NOT NULL,
+  title TEXT NOT NULL,
+  year INTEGER,
+  publisher TEXT
+)
