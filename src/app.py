c612236eca40db50297954593b10667471d74f07
Olavi Suojala <o.suojala@gmail.com>
2025-11-18 17:31:49 +0200

    refactoring

--- src/app.py
+++ src/app.py
@@ -4,15 +4,10 @@ from repositories.todo_repository import get_todos, create_todo, set_done
 from config import app, test_env
 from util import validate_todo
 
-@app.route("/")
-def index():
-    todos = get_todos()
-    unfinished = len([todo for todo in todos if not todo.done])
-    return render_template("index.html", todos=todos, unfinished=unfinished)
 
-@app.route("/references")
+@app.route("/")
 def references():
-    return render_template("references.html")
+    return render_template("index.html")
 
 @app.route("/create_book", methods=["POST"])
 def save():
@@ -27,27 +22,6 @@ def save():
 @app.route("/new_book")
 def new_book():
     return render_template("new_book.html")
-
-@app.route("/new_todo")
-def new():
-    return render_template("new_todo.html")
-
-@app.route("/create_todo", methods=["POST"])
-def todo_creation():
-    content = request.form.get("content")
-
-    try:
-        validate_todo(content)
-        create_todo(content)
-        return redirect("/")
-    except Exception as error:
-        flash(str(error))
-        return  redirect("/new_todo")
-
-@app.route("/toggle_todo/<todo_id>", methods=["POST"])
-def toggle_todo(todo_id):
-    set_done(todo_id)
-    return redirect("/")
 
 # testausta varten oleva reitti
 if test_env:
