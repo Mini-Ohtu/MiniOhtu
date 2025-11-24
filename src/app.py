19cf20ac1ae6493bfe94a0ef689ad00f2a5a8b32
Aura Pasila <aura.pasila@gmail.com>
2025-11-24 20:48:04 +0200

    Continued creating placeholder for editing

--- src/app.py
+++ src/app.py
@@ -1,7 +1,7 @@
 from urllib.parse import urlencode
 from flask import redirect, render_template, request, jsonify, url_for
 from db_helper import reset_db
-from repositories.reference_repository import get_references, create_references
+from repositories.reference_repository import get_references, create_references, update_reference
 from config import app, test_env
 from util import validate_reference_title, validate_reference_year
 
@@ -39,29 +39,27 @@ def reference_creation():
     # pylint: disable=W0703
     except Exception as error:
         query = urlencode({"error": str(error)})
-        return redirect(f"{url_for('new')}?{query}")
+        return redirect(f"{url_for("new")}?{query}")
 
-    #laitettu kommentteihin, kunnes lopullinen toteutus
 
-# @app.route("/edit_reference", methods=["POST"])
-# def edit_reference():
-    # citekey = request.form.get("citekey")
-    # author = request.form.get("author")
-    # title = request.form.get("title")
-    # year = request.form.get("year")
-    # publisher = request.form.get("publisher")
+@app.route("/edit_reference/<key>", methods=["POST"])
+def edit_reference(key):
+    citekey = key
+    author = request.form.get("author")
+    title = request.form.get("title")
+    year = request.form.get("year")
+    publisher = request.form.get("publisher")
 
-    #try:
-        # validate_reference_title(title)
-        # validate_reference_year(year)
-        # Huom. updatea ei vielä ole toteutettu
-        # update_reference(citekey, author, title, year_value, publisher)
-        # return redirect(url_for("index", reference_updated="true"))
+    try:
+        validate_reference_title(title)
+        validate_reference_year(year)
+        update_reference(citekey, author, title, year, publisher)
+        return redirect(url_for("/", reference_updated="true"))
 
-    # except Exception as error:
-        # query = urlencode({"error": str(error)})
-        # viite = get_reference(citekey)
-        # return render_template("edit_reference.html", viite=viite, error_message=query)
+    except ValueError as error:
+        query = urlencode({"error": str(error)})
+        return redirect(f"{url_for("/edit_reference")}?{query}")
+
 
 @app.route("/delete_reference/<key>", methods=["POST"])
 def reference_deletion(key):
