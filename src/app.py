1b47d16df5b1627f2d93c4ccd2ba162e32fd3c85
Aura Pasila <aura.pasila@gmail.com>
2025-11-25 19:00:49 +0200

    Fixing editing

--- src/app.py
+++ src/app.py
@@ -1,7 +1,13 @@
 from urllib.parse import urlencode
 from flask import redirect, render_template, request, jsonify, url_for
 from db_helper import reset_db
-from repositories.reference_repository import get_references, create_references, update_reference, delete_reference
+from repositories.reference_repository import (
+    delete_reference,
+    get_references,
+    create_references,
+    update_reference,
+    get_reference_by_key
+)
 from config import app, test_env
 from util import validate_reference_title, validate_reference_year
 
@@ -42,23 +48,36 @@ def reference_creation():
         return redirect(f"{url_for("new")}?{query}")
 
 
-@app.route("/edit_reference/<key>", methods=["POST"])
+@app.route("/edit_reference/<key>", methods=["GET", "POST"])
 def edit_reference(key):
+    if request.method == "GET":
+        viite = get_reference_by_key(key)
+        if viite is None:
+            return "Reference not found", 404
+        return render_template("edit_reference.html", viite=viite)
     citekey = key
     author = request.form.get("author")
     title = request.form.get("title")
     year = request.form.get("year")
     publisher = request.form.get("publisher")
 
+    error_message = None
+
     try:
         validate_reference_title(title)
         validate_reference_year(year)
-        update_reference(citekey, author, title, year, publisher)
-        return redirect(url_for("/", reference_updated="true"))
 
-    except ValueError as error:
-        query = urlencode({"error": str(error)})
-        return redirect(f"{url_for("/edit_reference")}?{query}")
+        update_reference(citekey, author, title, year, publisher)
+
+        viite = get_reference_by_key(key)
+        success_message = "Päivitys onnistui"
+        return render_template("edit_reference.html", viite=viite, success_message=success_message)
+
+    # pylint: disable=W0718
+    except Exception as error:
+        error_message = str(error)
+        viite = get_reference_by_key(key)
+        return render_template("edit_reference.html", viite=viite, error_message=error_message)
 
 
 @app.route("/delete_reference/<key>", methods=["POST"])
