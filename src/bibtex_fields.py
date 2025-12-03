71a0128514cb4b3ffff882c6b6ac61a59e67c22a
Artem Smirnov <learningartems@gmail.com>
2025-12-03 16:27:42 +0200

    done doi

--- src/bibtex_fields.py
+++ src/bibtex_fields.py
@@ -7,4 +7,52 @@ BIBTEX_FIELDS = {
         ["author", "title", "journal", "year"],
         ["volume", "number", "pages", "month", "doi", "note", "key", "tag"],
     ],
+    "booklet": [
+        ["title"],
+        ["author", "howpublished", "address", "month", "year", "note", "key", "tag"],
+    ],
+    "conference": [
+        ["author", "title", "booktitle", "year"],
+        ["editor", "volume", "number", "series", "pages", "address", "month", "organization", "publisher", "note", "key","tag"],
+    ],
+    "inbook": [
+        ["author", "editor", "title", "chapter", "pages", "publisher", "year"],
+        ["volume", "number", "series", "type", "address", "edition", "month", "note", "key", "tag"],
+    ],
+    "incollection": [
+        ["author", "title", "booktitle", "publisher", "year"],
+        ["editor", "volume", "number", "series", "type", "chapter", "pages", "address", "edition", "month", "note", "key", "tag"],
+    ],
+    "inproceedings": [
+        ["author", "title", "booktitle", "year"],
+        ["editor", "volume", "number", "series", "pages", "address", "month", "organization", "publisher", "note", "key", "tag"],
+    ],
+    "manual": [
+        ["title"],
+        ["author", "organization", "address", "edition", "month", "year", "note", "key", "tag"],
+    ],
+    "mastersthesis": [
+        ["author", "title", "school", "year"],
+        ["type", "address", "month", "note", "key", "tag"],
+    ],
+    "misc": [
+        [],
+        ["author", "title", "howpublished", "month", "year", "note", "key", "tag"],
+    ],
+    "phdthesis": [
+        ["author", "title", "school", "year"],
+        ["type", "address", "month", "note", "key", "tag"],
+    ],
+    "proceedings": [
+        ["title", "year"],
+        ["editor", "volume", "number", "series", "address", "month", "publisher", "organization", "note", "key", "tag"],
+    ],
+    "techreport": [
+        ["author", "title", "institution", "year"],
+        ["type", "number", "address", "month", "note", "key", "tag"],
+    ],
+    "unpublished": [
+        ["author", "title", "note"],
+        ["month", "year", "key", "tag"],
+    ],
 }
